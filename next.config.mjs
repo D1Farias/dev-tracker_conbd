3439df4cde4e731103a372057981b5a268ae453b
D1Farias <diefal1010@gmail.com>
2026-03-31 09:45:53 -0300

    conf ngrok

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ['kaydence-uncompelled-izola.ngrok-free.dev'],
   typescript: {
     ignoreBuildErrors: true,
   },
